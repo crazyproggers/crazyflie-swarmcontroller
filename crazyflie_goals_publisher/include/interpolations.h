75bb65933e94744e0d72f3d1da81399eb42f5660
YuMarkova <tealhelix@gmail.com>
2017-10-06 17:06:35 +0200

    taken out interpolations

---
+++ crazyflie_goals_publisher/include/interpolations.h
@@ -0,0 +1,12 @@
+#ifndef INTERPOLATION_H
+#define INTERPOLATION_H
+
+#include <tf/transform_listener.h>
+#include <vector>
+#include "goal.h"
+
+std::vector<Goal> interpolate(const Goal &goal1, const Goal &goal2);
+
+std::vector<Goal> createSpline(std::vector<Goal> goals, double step = 0.01);
+
+#endif // INTERPOLATION_H
