21740ace1e3d421232e834bfda44c23eadee599d
YuMarkova <tealhelix@gmail.com>
2017-10-24 15:26:49 +0200

    minor improvements in interpolations.h

--- crazyflie_goals_publisher/include/interpolations.h
+++ crazyflie_goals_publisher/include/interpolations.h
@@ -4,11 +4,16 @@
 #include <list>
 #include "goal.h"
 
-// Linear interpolation between two goals
-std::list<Goal> interpolate(const Goal &goal1, const Goal &goal2, double distance = 0.01);
 
 /*
- *  Catmull-Rom interpolation
+ * Linear interpolation between two goals
+ * Parameter distance = 0.05 - it is distance between intermediate goals
+ */
+std::list<Goal> interpolate(const Goal &goal1, const Goal &goal2, double distance = 0.05);
+
+
+/*
+ *  Centripetal Catmull-Rom interpolation
  *  https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
  */
 std::list<Goal> createSpline(std::list<Goal> goals, double step = 0.01);
